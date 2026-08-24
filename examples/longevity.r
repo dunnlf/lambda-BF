0bc499b0a454eafa06a6a733e546c32b89fb8eb6
SheepMaySafelyGraze <114660159+SheepMaySafelyGraze@users.noreply.github.com>
2026-08-24 21:14:58 +0100

    feat: longevity example

--- examples/longevity.r
+++ examples/longevity.r
@@ -24,6 +24,20 @@ write.csv(species, "lambda-BF-example-data\\zhang\\zhang-species.csv", row.names
 # read tree from timetree
 tree <- read.tree("lambda-BF-example-data\\zhang\\zhang-species.tre")
 
+# bayes factor on tree used by Zhang et al. (2026)
 LQ <- setNames(data$Longevity.quotient..LQ., sapply(species,
                                 function(x) gsub(" ", "_", x)))
+
+if (length(setdiff(names(LQ), tree$tip.label)) != 0) {
+    print("Warning: some species in LQ data not found in trees")}
+
 bf <- lambdaBF(tree, LQ)
+
+
+# on a sample of 100 trees from VertLife
+trees_100 <- read.nexus("lambda-BF-example-data\\zhang\\mammal-trees-100.nex")
+
+if (length(setdiff(names(LQ), trees_100[[1]]$tip.label)) != 0) {
+    print("Warning: some species in LQ data not found in trees")}
+
+bf_100 <- lambdaBF(trees_100, LQ, return_trace = TRUE)
